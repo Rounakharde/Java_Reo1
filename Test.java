49d028d54b85bdac279cece1bb1d6a75cde4cacc
rounak <rounakharde86@gmail.com>
2024-06-14 23:39:21 +0530

    Test.java is add by Dev 1

--- Test.java
+++ Test.java
@@ -2,6 +2,17 @@ class Test
 {
  public static void main(String [] args)
 	{
+	
 	System.out.println("Hello");
+	Test t=new Test();	
+	t.m2(2,3);
+	}
+	public int m2(int a,int b)
+	{
+		int r=a+b;
+		System.out.println(r);
+		return(r);
+
+	
 	}
 }
